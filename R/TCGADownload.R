f90f90883eedb667c9827ed4f50ecfc7469f4503
Edward <1749748955@qq.com>
2021-05-23 19:06:37 +0800

    first commit

---
+++ R/TCGADownload.R
@@ -0,0 +1,12 @@
+
+#' TCGA Download
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' TCGA()
+TCGA <- function(){
+  message("Hello TCGA")
+}
