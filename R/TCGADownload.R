431e1402865d8d5bcb3f024f26424c7816627916
Edward <1749748955@qq.com>
2021-05-23 20:58:22 +0800

    update

--- R/TCGADownload.R
+++ R/TCGADownload.R
@@ -1,3 +1,32 @@
+library("optparse")
+library(TCGAbiolinks)
+library(tidyverse)
+library(reshape2)
+
+
+
+
+
+report <- function(...){
+  if(!exists("IS_DEBUG")){
+    IS_DEBUG <<-T
+  }
+  if(!exists("LOG_FILENAME")){
+    LOG_FILENAME <<- "report.txt"
+  }
+  if(exists("IS_EMPTY")&&IS_EMPTY==T){
+    cat("",file=LOG_FILENAME)
+  }
+  report <- function(...){
+    if(IS_DEBUG){
+      cat(...,"\n")
+    }else{
+      cat(...,"\n",file=LOG_FILENAME,append=T,sep="")
+    }
+  }
+}
+
+
 
 #' TCGA Download
 #'
---
+++ R/test.R
@@ -0,0 +1,9 @@
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' testF()
+testF <- function(){
+  message(TCGA())
+}
